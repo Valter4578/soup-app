ea872ced56a495fbcb64fd916bfb549b6b7b017d
Maksim Alekseev <mrvalter566@gmail.com>
2025-03-01 00:04:59 +0700

    Init commit

---
+++ Soup/SoupApp.swift
@@ -0,0 +1,17 @@
+//
+//  SoupApp.swift
+//  Soup
+//
+//  Created by Максим Алексеев  on 01.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct SoupApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SoupTests/SoupTests.swift
@@ -0,0 +1,17 @@
+//
+//  SoupTests.swift
+//  SoupTests
+//
+//  Created by Максим Алексеев  on 01.03.2025.
+//
+
+import Testing
+@testable import Soup
+
+struct SoupTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
