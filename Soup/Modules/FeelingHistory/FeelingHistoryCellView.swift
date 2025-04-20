161649642da10a14d4a9bb1337d0ee49ecd7e2a3
Maksim Alekseev <mrvalter566@gmail.com>
2025-04-20 17:58:37 +0700

    Some changes

---
+++ Soup/Modules/FeelingHistory/FeelingHistoryCellView.swift
@@ -0,0 +1,18 @@
+//
+//  FeelingHistoryCellView.swift
+//  Soup
+//
+//  Created by Максим Алексеев  on 06.04.2025.
+//
+
+import SwiftUI
+
+struct FeelingHistoryCellView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FeelingHistoryCellView()
+}
